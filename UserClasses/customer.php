3f852219c79dfa10fe0c6272f008fea3b7470bd4
alex-24-cyber <B00151494@mytudublin.ie>
2024-03-08 17:43:06 +0000

    UserClasses

---
+++ UserClasses/customer.php
@@ -0,0 +1,40 @@
+<?php
+
+class Customer extends User {
+    protected $Fname;
+    protected $Sname;
+    protected $DOB;
+    protected $EirCode;
+    protected $Phone;
+
+    public function __construct($UserID, $Email, $Password, $Fname, $Sname, $DOB, $EirCode, $Phone) {
+        parent::__construct($UserID, $Email, $Password);
+        $this->Fname = $Fname;
+        $this->Sname = $Sname;
+        $this->DOB = $DOB;
+        $this->EirCode = $EirCode;
+        $this->Phone = $Phone;
+    }
+
+    public function getFname() {
+        return $this->Fname;
+    }
+
+    public function getSname() {
+        return $this->Sname;
+    }
+
+    public function getDOB() {
+        return $this->DOB;
+    }
+
+    public function getEirCode() {
+        return $this->EirCode;
+    }
+
+    public function getPhone() {
+        return $this->Phone;
+    }
+}
+
+?>

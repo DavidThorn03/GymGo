eb04ce6713cd58b05c9b2215a33811d96b44479b
vUltiQ <70598538+vUltiQ@users.noreply.github.com>
2024-03-20 14:23:20 +0000

    Add files via upload

---
+++ public/OrderConfirmationController.php
@@ -0,0 +1,32 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+require_once '../ProductClasses/Order.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Confirmation</title>
+    <link rel="stylesheet" href="../public/css/order.css">
+</head>
+<body>
+    <div class="container2">
+        <h1>Order Confirmation</h1>
+        <?php
+        require '../dbQueries/productQueries.php';
+        require '../ProductClasses/Product.php'; 
+
+
+        // FIXED USER ID FOR NOW
+        $userID = 1;
+        $Order = new Order($userID);
+        $Order->displayConfirmation();
+        ?>
+
+        <a href="products.php">Continue Shopping</a>
+    </div>
+</body>
+</html>

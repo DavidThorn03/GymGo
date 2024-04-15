d69513e72d5f67fd7841210bc36eb375da15f8f7
vUltiQ <70598538+vUltiQ@users.noreply.github.com>
2024-04-15 21:24:05 +0100

    Add files via upload

--- public/OrderConfirmationController.php
+++ public/OrderConfirmationController.php
@@ -3,25 +3,26 @@ require 'templates/header.php';
 require_once '../ProductClasses/Order.php';
 require_once '../dbQueries/productQueries.php';
 require_once '../ProductClasses/Product.php';
-require_once '../ProductClasses/ShoppingCart.php'; 
+require_once '../ProductClasses/ShoppingCart.php';
+require_once '../UserClasses/customer.php';
 
-?>
-    <div class="container2">
-        <h1>Order Confirmation</h1>
-        <?php
-        //testing
-        $userID = 1;
-        
-        // new shopping cart object
-        $cart = new ShoppingCart();
-        
-        // pass both userID and cart to the Order constructor
-        $Order = new Order($userID, $cart);
-        
-        // Display order confirmation
-        $Order->displayConfirmation();
-        ?>
 
-        <a href="products.php">Continue Shopping</a>
-    </div>
-<?php require 'templates/footer.php'; ?>
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit;
+}
+
+$user = unserialize($_SESSION['user']);
+if ($user instanceof Customer) {
+    $userId = $user->getUserID();
+}
+if ($userId) {
+    $order->displayConfirmation();
+}
+
+echo '<div class="container2">';
+echo '<h1>Order Confirmation</h1>';
+echo '<a href="products.php">Continue Shopping</a>';
+echo '</div>';
+
+require 'templates/footer.php';

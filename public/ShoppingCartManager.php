3b4226651f81e6a6cd8978cb0a24d2fd1eb4c7fa
alex-24-cyber <B00151494@mytudublin.ie>
2024-04-23 21:07:58 +0100

    RedundantBtn

--- public/ShoppingCartManager.php
+++ public/ShoppingCartManager.php
@@ -3,10 +3,6 @@ require 'templates/header.php';
 require_once '../ProductClasses/ShoppingCart.php';
 ?>
 
-<!-- Back to Shopping link/button -->
-<div style="text-align: center; margin-top: 20px;">
-    <a href="products.php" class="btn btn-primary">Back to Shopping</a>
-</div>
 
 <?php
 $cart = new ShoppingCart();
